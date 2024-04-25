8ea8f8f9db8683c22b0f94a2deee091cdc6a3b1e
EnanaAtomica <zcabrera@lasalleflorida.edu.ar>
2024-04-25 11:35:06 -0300

    Mi primer commit :D

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path'); // Import path module
+const app = express();
+const port = 43922;
+// Allow all cross-origin requests
+app.use(cors());
+// Middleware to parse JSON bodies
+app.use(express.json());
+// Nuestra aplicación esta lista para recibir peticiones
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+app.get('/', (req, res) => {
+    console.log(__dirname)
+    res.sendFile(path.join(__dirname, 'index.html'));
+    });
+    try{
+        // algo
+        } catch(e){
+        res.status(500).send({'error': 'Internal server error'})
+        }
